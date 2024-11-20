82d619d2f8102ab98728325b006853b2de4b7841
ICT Business Executive <desmond@liozio.com>
2024-11-20 13:11:21 +0100

    Vercel Config #2

--- LREDC/settings.py
+++ LREDC/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'LREDC.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -117,7 +117,7 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'LiozioMainApp/static/'
+STATIC_URL = 'main/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
